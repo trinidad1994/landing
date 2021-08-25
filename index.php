87278a9ea02ae8eeb10e7ad9caa815216279baa6
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-08-26 06:37:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,41 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.1.1">
-    <title>Watch Sexy Girls Nude Dancing for You</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha512-MoRNloxbStBcD8z3M/2BmnT+rg4IsMxPkXaGh2zD6LGNNFE80W3onsAhRcMAMrSoyWL9xD7Ert0men7vR8LUZg==" crossorigin="anonymous" />
-    <!-- Custom styles for this template -->
-    <link href="style.css" rel="stylesheet">
-  </head>
-    <body>
-<header>
-  <div class="overlay"></div>
-  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-    <source src="video/girl-dancing.mp4" type="video/mp4">
-  </video>
-  <div class="container h-100">
-    <div class="d-flex h-100 text-center align-items-center">
-      <div class="w-100 text-white">
-        <h1 class="display-3">LIVE ONLINE DATING</h1>
-        <p class="lead mb-0">Latest Porn Videos ?? LIVE CHAT Meet your next sex buddy. Single babes in your neighborhood!</p>
-        <p>
-            <a href="https://strawberry7705189.brizy.site/">CONTINUE</a>
-        </p>
-      </div>
-    </div>
-  </div>
-</header>
+<?php
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha512-M5KW3ztuIICmVIhjSqXe01oV2bpe248gOxqmlcYrEzAvws7Pw3z6BK0iGbrwvdrUQUhi3eXgtxp5I8PDo9YfjQ==" crossorigin="anonymous"></script>
-</html>
+// Genero: ProgramasProgramacion.com
+echo "<!doctype html>\n";
+echo "<html lang=\"en\">\n";
+echo "  <head>\n";
+echo "    <meta charset=\"utf-8\">\n";
+echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n";
+echo "    <meta name=\"description\" content=\"\">\n";
+echo "    <meta name=\"author\" content=\"Mark Otto, Jacob Thornton, and Bootstrap contributors\">\n";
+echo "    <meta name=\"generator\" content=\"Jekyll v4.1.1\">\n";
+echo "    <title>Watch Sexy Girls Nude Dancing for You</title>\n";
+echo "    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha512-MoRNloxbStBcD8z3M/2BmnT+rg4IsMxPkXaGh2zD6LGNNFE80W3onsAhRcMAMrSoyWL9xD7Ert0men7vR8LUZg==\" crossorigin=\"anonymous\" />\n";
+echo "    <!-- Custom styles for this template -->\n";
+echo "    <link href=\"style.css\" rel=\"stylesheet\">\n";
+echo "  </head>\n";
+echo "    <body>\n";
+echo "<header>\n";
+echo "  <div class=\"overlay\"></div>\n";
+echo "  <video playsinline=\"playsinline\" autoplay=\"autoplay\" muted=\"muted\" loop=\"loop\">\n";
+echo "    <source src=\"video/girl-dancing.mp4\" type=\"video/mp4\">\n";
+echo "  </video>\n";
+echo "  <div class=\"container h-100\">\n";
+echo "    <div class=\"d-flex h-100 text-center align-items-center\">\n";
+echo "      <div class=\"w-100 text-white\">\n";
+echo "        <h1 class=\"display-3\">LIVE ONLINE DATING</h1>\n";
+echo "        <p class=\"lead mb-0\">Latest Porn Videos ?? LIVE CHAT Meet your next sex buddy. Single babes in your neighborhood!</p>\n";
+echo "        <p>\n";
+echo "            <a href=\"https://strawberry7705189.brizy.site/\">CONTINUE</a>\n";
+echo "        </p>\n";
+echo "      </div>\n";
+echo "    </div>\n";
+echo "  </div>\n";
+echo "</header>\n";
+echo "\n";
+echo "<script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script>\n";
+echo "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js\" integrity=\"sha512-M5KW3ztuIICmVIhjSqXe01oV2bpe248gOxqmlcYrEzAvws7Pw3z6BK0iGbrwvdrUQUhi3eXgtxp5I8PDo9YfjQ==\" crossorigin=\"anonymous\"></script>\n";
+echo "</html>\n";
+echo "\n";
+
+?>
